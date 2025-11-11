1a3dec19488bdada8cc8457146c557f62b6a1c5f
shimiharu1012 <shimizu1012@ezweb.ne.jp>
2025-11-11 13:55:52 +0900

    sqlite->tinyDBに変更

--- chat.py
+++ chat.py
@@ -2,7 +2,7 @@ import os
 import dotenv
 import google.genai as genai
 from halo import Halo
-import sqlite3
+from tinydb import TinyDB
 from InquirerPy import inquirer
 
 # .envファイルから環境変数を読み込む
@@ -11,19 +11,9 @@ DB_DIR_PATH=os.getenv("DB_DIR_PATH")
 
 def open_database(filename):
     if filename=="new":
-        filename=input("Enter the room name: ")
-        connection = sqlite3.connect(f'{DB_DIR_PATH}/{filename}.db')
-        cursor = connection.cursor()
-        cursor.execute('CREATE TABLE history(id INTEGER PRIMARY KEY AUTOINCREMENT, role STRING, text STRING, created_at DATETIME)')  # tableを作成する指示
-        connection.commit()
-        cursor.close()
-        return connection
-    else:
-        dbname = f'{DB_DIR_PATH}/{filename}'
-
-    connection=sqlite3.connect(dbname)
-
-    return connection
+        filename=input("Enter the room name: ")+".json"
+        
+    return TinyDB(f'{DB_DIR_PATH}/{filename}')
 
 def configure_api():
     # 環境変数からGoogle APIキーを取得
@@ -34,13 +24,13 @@ def configure_api():
     # APIキーを用いてGoogle AIクライアントを初期化
     return genai.Client(api_key=api_key)
 
+def add_to_history(response,db):
+    content={
+        "role":response.candidates[0].content.role,
+        "parts":[{"text":response.candidates[0].content.parts[0].text}]
+    }
+    db.table('history').insert(content)
 
-def add_to_history(response,connection,cursor):
-    role=response.candidates[0].content.role
-    text=response.candidates[0].content.parts[0].text
-
-    cursor.execute("INSERT INTO history (role, text) VALUES (?, ?)", (role, text))
-    connection.commit()
 
 def format_history(rows):
     formatted_history=[]
@@ -53,48 +43,45 @@ def format_history(rows):
 
 
 def main():
-    db_files=os.listdir(DB_DIR_PATH)
-    db_choices=[
-        {"name": db_file.rstrip(".db"), "value": db_file} for db_file in db_files
-    ]
-    db_choices.append({"name": "Create new database", "value": "new"})
-
-    filename=inquirer.select(
-        message="Select the room:",
-        choices=db_choices,
-        default=db_choices[0],
-        pointer="➤"
-    ).execute()
-
-    # SQLiteのDBへ接続
-    connection=open_database(filename)
-    cursor=connection.cursor()
-    
-    rows=cursor.execute("SELECT * FROM history").fetchall()
-    history=format_history(rows)
-    
-    client = configure_api()
-    spinner = Halo(text='Thinking...', spinner='dots')
-    
-    chat = client.chats.create(model="gemini-2.0-flash", history=history)
-
     while True:
-        try:
-            user_input = input("You: ")
-            if user_input.lower() == "exit":
-                break
-            spinner.start()
-            response = chat.send_message(user_input)
-            spinner.stop()
-            print("Gemini: ", response.text)
-            add_to_history(response,connection,cursor)
-        except Exception as e:
-            print(f"エラーが発生しました: {e}")
-            break
+        db_files=os.listdir(DB_DIR_PATH)
     
-    cursor.close()
-    connection.close()
+        db_choices=[
+            {"name": db_file.rstrip(".json"), "value": db_file} for db_file in db_files
+        ]
+        db_choices.append({"name": "Create new database", "value": "new"})
+        
+        filename=inquirer.select(
+            message="Select the room:",
+            choices=db_choices,
+            default=db_choices[0],
+            pointer="➤"
+        ).execute()
 
+        # TinyDBの接続
+        db=open_database(filename)
+        history=db.table('history').all()
+        
+        client = configure_api()
+        spinner = Halo(text='Thinking...', spinner='dots')
+        
+        chat = client.chats.create(model="gemini-2.0-flash", history=history)
+
+        while True:
+            try:
+                user_input = input("You: ")
+                if user_input.lower() == "exit":
+                    break
+                spinner.start()
+                response = chat.send_message(user_input)
+                spinner.stop()
+                print("Gemini: ", response.text)
+                add_to_history(response,db)
+            except Exception as e:
+                print(f"エラーが発生しました: {e}")
+                break
+        
+        db.close()
 
 if __name__ == "__main__":
     main()
